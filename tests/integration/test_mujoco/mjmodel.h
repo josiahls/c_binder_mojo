22e506c1b3d31d960016eb5b71bcd394f568f130
josiahls <jklaivins@gmail.com>
2025-03-30 16:23:42 +0000

    (chore): simplified the ast even more

--- tests/integration/test_mujoco/mjmodel.h
+++ tests/integration/test_mujoco/mjmodel.h
@@ -1,76 +1,36 @@
 #ifndef MUJOCO_MJMODEL_H_
 #define MUJOCO_MJMODEL_H_
 
-
-struct mjLROpt_ {                 // options for mj_setLengthRange()
-  // flags
-  int mode;                       // which actuators to process (mjtLRMode)
+struct mjLROpt_ {
+  int mode;
 };
 typedef struct mjLROpt_ mjLROpt;
 
-
-//---------------------------------- mjVFS ---------------------------------------------------------
-
-struct mjVFS_ {                               // virtual file system for loading from memory
-  void* impl_;                                // internal pointer to VFS memory
+struct mjVFS_ {
+  void* impl_;
 };
 typedef struct mjVFS_ mjVFS;
 
-//---------------------------------- mjOption ------------------------------------------------------
-
-struct mjOption_ {                // physics options
-
-  int sdf_iterations;             // max number of iterations for gradient descent
+struct mjOption_ {
+  int sdf_iterations;
 };
 typedef struct mjOption_ mjOption;
 
-
-//---------------------------------- mjVisual ------------------------------------------------------
-
-struct mjVisual_ {                // visualization options
-  struct {                        // global parameters
-    int orthographic;             // is the free camera orthographic (0: no, 1: yes)
+struct mjVisual_ {
+  struct {
+    int orthographic;
   } global;
-
-  struct {                        // rendering quality
-    int   shadowsize;             // size of shadowmap texture
-  } quality;
-
-  struct {                        // head light
-    float ambient[3];             // ambient rgb (alpha=1)
-  } headlight;
-
-  struct {                        // mapping
-    float stiffness;              // mouse perturbation stiffness (space->force)
-  } map;
-
-  struct {                        // scale of decor elements relative to mean body size
-    float forcewidth;             // width of force arrow
-  } scale;
-
-  struct {                        // color of decor elements
-    float bvactive[4];            // active bounding volume
-  } rgba;
 };
 typedef struct mjVisual_ mjVisual;
 
-
-//---------------------------------- mjStatistic ---------------------------------------------------
-
-struct mjStatistic_ {             // model statistics (in qpos0)
-  mjtNum meaninertia;             // mean diagonal inertia
-
+struct mjStatistic_ {
+  mjtNum meaninertia;
 };
 typedef struct mjStatistic_ mjStatistic;
 
-
-//---------------------------------- mjModel -------------------------------------------------------
-
 struct mjModel_ {
-  // ------------------------------- sizes
-  // sizes needed at mjModel construction
-  int nq;                         // number of generalized coordinates = dim(qpos)
+  int nq;
 };
 typedef struct mjModel_ mjModel;
 
-#endif  // MUJOCO_MJMODEL_H_
+#endif

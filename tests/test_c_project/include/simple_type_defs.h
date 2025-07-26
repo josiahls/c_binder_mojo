59784c9f493592837e26b0a295b3bd10917be207
josiahls <jklaivins@gmail.com>
2025-07-26 17:21:03 +0000

    added: test_c_project for testing C code

--- tests/test_c_project/include/simple_type_defs.h
+++ tests/test_c_project/include/simple_type_defs.h
@@ -16,36 +16,36 @@
 #define mjMAXIMP        0.9999    // maximum constraint impedance
 #define mjMAXCONPAIR    50        // maximum number of contacts per geom pair
 
-// Simple type definitions
+// Simple type definitions and function declarations for Mojo C bindings
 typedef int simple_type_int;
-simple_type_int get_simple_type_int(simple_type_int a) { return a; }
+simple_type_int get_simple_type_int(simple_type_int a);
 
 typedef float simple_type_float;
-simple_type_float get_simple_type_float(simple_type_float a) { return a; }
+simple_type_float get_simple_type_float(simple_type_float a);
 
 typedef double simple_type_double;
-simple_type_double get_simple_type_double(simple_type_double a) { return a; }
+simple_type_double get_simple_type_double(simple_type_double a);
 
 typedef unsigned char simple_type_byte; 
-simple_type_byte get_simple_type_byte(simple_type_byte a) { return a; }
+simple_type_byte get_simple_type_byte(simple_type_byte a);
 
 typedef unsigned short simple_type_short;
-simple_type_short get_simple_type_short(simple_type_short a) { return a; }
+simple_type_short get_simple_type_short(simple_type_short a);
 
 typedef unsigned int simple_type_uint;
-simple_type_uint get_simple_type_uint(simple_type_uint a) { return a; }
+simple_type_uint get_simple_type_uint(simple_type_uint a);
 
 typedef unsigned long long simple_type_long_long;
-simple_type_long_long get_simple_type_long_long(simple_type_long_long a) { return a; }
+simple_type_long_long get_simple_type_long_long(simple_type_long_long a);
 
 typedef unsigned long simple_type_long;
-simple_type_long get_simple_type_long(simple_type_long a) { return a; }
+simple_type_long get_simple_type_long(simple_type_long a);
 
 typedef unsigned long long simple_type_ulong_long;
-simple_type_ulong_long get_simple_type_ulong_long(simple_type_ulong_long a) { return a; }
+simple_type_ulong_long get_simple_type_ulong_long(simple_type_ulong_long a);
 
 typedef unsigned long simple_type_ulong;
-simple_type_ulong get_simple_type_ulong(simple_type_ulong a) { return a; }
+simple_type_ulong get_simple_type_ulong(simple_type_ulong a);
 
 
 

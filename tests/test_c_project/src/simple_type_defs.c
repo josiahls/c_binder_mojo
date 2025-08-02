c244ac98b9d79445432951bdfd452f2053b540f3
josiahls <jklaivins@gmail.com>
2025-08-02 19:13:08 +0000

    refactor: cleanup the test_c_project to use the new primitive types.

--- tests/test_c_project/src/simple_type_defs.c
+++ tests/test_c_project/src/simple_type_defs.c
@@ -1,22 +1,46 @@
 #include "simple_type_defs.h"
 
 // Function implementations for Mojo C bindings
-simple_type_int get_simple_type_int(simple_type_int a) { return a; }
-
-simple_type_float get_simple_type_float(simple_type_float a) { return a; }
-
-simple_type_double get_simple_type_double(simple_type_double a) { return a; }
-
+/** Byte types */
 simple_type_byte get_simple_type_byte(simple_type_byte a) { return a; }
 
+simple_type_sbyte get_simple_type_sbyte(simple_type_sbyte a) { return a; }
+
+simple_type_ubyte get_simple_type_ubyte(simple_type_ubyte a) { return a; }
+
+/** Short types */
 simple_type_short get_simple_type_short(simple_type_short a) { return a; }
+
+simple_type_ushort get_simple_type_ushort(simple_type_ushort a) { return a; }
+
+simple_type_sshort get_simple_type_sshort(simple_type_sshort a) { return a; }
+
+/** Int types */
+simple_type_int get_simple_type_int(simple_type_int a) { return a; }
 
 simple_type_uint get_simple_type_uint(simple_type_uint a) { return a; }
 
-simple_type_long_long get_simple_type_long_long(simple_type_long_long a) { return a; }
+simple_type_sint get_simple_type_sint(simple_type_sint a) { return a; }
 
+/** Long types */
 simple_type_long get_simple_type_long(simple_type_long a) { return a; }
+
+simple_type_ulong get_simple_type_ulong(simple_type_ulong a) { return a; }
+
+simple_type_slong get_simple_type_slong(simple_type_slong a) { return a; }
+
+/** Long long types */
+simple_type_long_long get_simple_type_long_long(simple_type_long_long a) { return a; }
 
 simple_type_ulong_long get_simple_type_ulong_long(simple_type_ulong_long a) { return a; }
 
-simple_type_ulong get_simple_type_ulong(simple_type_ulong a) { return a; } 
+simple_type_slong_long get_simple_type_slong_long(simple_type_slong_long a) { return a; }
+
+/** Float types */
+simple_type_float get_simple_type_float(simple_type_float a) { return a; }
+
+/** Double types */
+simple_type_double get_simple_type_double(simple_type_double a) { return a; }
+
+/** Long double types */
+simple_type_long_double get_simple_type_long_double(simple_type_long_double a) { return a; }

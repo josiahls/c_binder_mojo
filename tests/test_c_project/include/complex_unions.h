465256c299b136600b131811be21894bf467fdc8
josiahls <jklaivins@gmail.com>
2025-08-17 20:29:46 +0000

    Add C_Union type

---
+++ tests/test_c_project/include/complex_unions.h
@@ -0,0 +1,44 @@
+#define mjMAXUIMULTI    35
+#define mjMAXUIEDIT     7
+#define mjMAXUIRECT     25
+#define mjMAXUINAME     40
+
+
+struct mjuiItemSingle_ {
+    int modifier;
+    int shortcut;
+};
+
+
+struct mjuiItemMulti_ {
+    int nelem;
+    char name[mjMAXUIMULTI][mjMAXUINAME];
+};
+
+
+struct mjuiItemSlider_ {
+    double range[2];
+    double divisions;
+};
+
+
+struct mjuiItemEdit_ {
+    int nelem;
+    double range[mjMAXUIEDIT][2];
+};
+
+
+struct mjuiItem_ {
+    int type;
+    char name[mjMAXUINAME];
+
+    union {
+      struct mjuiItemSingle_ single;
+      struct mjuiItemMulti_ multi;
+      struct mjuiItemSlider_ slider;
+      struct mjuiItemEdit_ edit;
+    };
+
+    int skip;
+};
+typedef struct mjuiItem_ mjuiItem;
---
+++ tests/test_c_project/mojo/test_complex_unions.mojo
@@ -0,0 +1,10 @@
+from complex_structs import *
+from testing import assert_almost_equal, assert_equal
+
+
+fn test_complex_structs() raises:
+    var complex_structs = complex_structs()
+
+
+fn main() raises:
+    test_complex_structs()

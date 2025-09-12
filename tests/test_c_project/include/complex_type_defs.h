2a9085750997fc8a04a3dc43197d71359d3017f0
josiahls <jklaivins@gmail.com>
2025-09-12 19:22:50 +0000

    feat: Updated complex_type_defs.h with complicated at_exit function

--- tests/test_c_project/include/complex_type_defs.h
+++ tests/test_c_project/include/complex_type_defs.h
@@ -6,3 +6,11 @@ typedef const int *complex_typedef_1[10];
 typedef const int *(*complex_typedef_2)(const int *, int *);
 // fn (read *UnsafePointer[Int8]) -> NoneType
 typedef void (*complex_typedef_3)(const char *, ...);
+
+
+// Simplest possible definitions - just empty macros
+#define __THROW
+#define __nonnull(params)
+
+extern int on_exit (void (*__func) (int __status, void *__arg), void *__arg)
+     __THROW __nonnull ((1));

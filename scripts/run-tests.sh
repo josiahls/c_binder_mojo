78b8427087a80b8ed282f6034b0befb6d9d6c76d
josiahls <jklaivins@gmail.com>
2025-09-21 21:04:58 +0000

    chore: Exclude assertion expectations for now.

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -9,7 +9,8 @@ run_cmd() {
 
 run_cmd pixi run mojo run -I . $C_BINDER_INCLUDES tests/test_simple_nodes.mojo
 
-run_cmd pixi run generate_example_bindings_and_test rte_mbuf_core true
+# We don't have a unit test to assert this.
+# run_cmd pixi run generate_example_bindings_and_test rte_mbuf_core true
 
 run_cmd pixi run generate_example_bindings_and_test simple_type_defs
 
@@ -17,4 +18,5 @@ run_cmd pixi run generate_example_bindings_and_test complex_structs
 
 run_cmd pixi run generate_example_bindings_and_test complex_unions
 
-run_cmd pixi run generate_example_bindings_and_test complex_type_defs true
+# We don't have a unit test to assert this.
+# run_cmd pixi run generate_example_bindings_and_test complex_type_defs true

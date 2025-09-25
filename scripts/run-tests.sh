b357a31c802e12f2ddb6d0aa37c3272c3120226b
josiahls <jklaivins@gmail.com>
2025-09-25 15:48:08 +0000

    fix: Cleanup run-tests.sh

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -7,7 +7,7 @@ run_cmd() {
 }
 
 
-run_cmd pixi run mojo run -I . $C_BINDER_INCLUDES tests/test_simple_nodes.mojo
+run_cmd pixi run mojo run $C_BINDER_INCLUDES tests/test_simple_nodes.mojo
 
 # We don't have a unit test to assert this.
 # run_cmd pixi run generate_example_bindings_and_test rte_mbuf_core true

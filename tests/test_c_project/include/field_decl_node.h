a62be991a72e4d576225a76d153493c4a3e74488
josiahls <jklaivins@gmail.com>
2025-09-30 18:46:05 +0000

    feat: Add unnamed struct to field decl node test

--- tests/test_c_project/include/field_decl_node.h
+++ tests/test_c_project/include/field_decl_node.h
@@ -7,3 +7,11 @@ struct simple_field_struct {
 struct simple_field_struct identity_function(struct simple_field_struct input);
 
 struct simple_field_struct* identity_function_pointer(struct simple_field_struct* input);
+
+struct {
+  unsigned long long int __value64;
+  struct
+  {
+    unsigned int __low;
+  } __value32;
+} complex_field_struct;

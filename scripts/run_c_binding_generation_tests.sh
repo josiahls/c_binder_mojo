003278197bdd9ddfeaa1d5a614ded342e248fcd5
josiahls <jklaivins@gmail.com>
2025-10-14 19:11:16 +0000

    fixup

--- scripts/run_c_binding_generation_tests.sh
+++ scripts/run_c_binding_generation_tests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-# Get test directory from second argument
+# Get test directory from first argument
 if [ -z "$1" ]; then
-    test_dir="tests/"
+    test_dir="tests/test_c_project/include/"
     echo "Finding tests in $test_dir"
 else
     test_dir="$1"
@@ -12,11 +12,11 @@ fi
 any_failed=false
 echo "### ------------------------------------------------------------- ###"
 while IFS= read -r test_file; do
-    if ! pixi run mojo run $C_BINDER_TEST_INCLUDES "$test_file"; then
+    if ! pixi run generate_example_bindings "$test_file"; then
         any_failed=true
     fi
     echo "### ------------------------------------------------------------- ###"
-done < <(find "$test_dir" -name "test_*.mojo" -type f | sort)
+done < <(find "$test_dir" -name "*.h" -type f | sort)
 
 if [ "$any_failed" = true ]; then
     exit 1
